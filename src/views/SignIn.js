1fb59e2391b6deacfbbbb2ebd60ee4a0462bb0bf
michalfirszt <michalfiszu@gmail.com>
2022-01-28 15:25:57 +0100

    Save user data

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,38 +1,51 @@
 import React, { useCallback, useState } from 'react';
+import { useLocalStorage } from 'react-use';
 
 import { Button, Card, Input } from '../components';
 
 export const SignIn = () => {
+  const [usernameKey, setUsernameKey, removeUsernameKey] =
+    useLocalStorage('username');
   const [username, setUsername] = useState('');
 
   const handleOnSubmit = useCallback(
     (event) => {
       event.preventDefault();
 
-      console.log(username);
+      setUsernameKey(username);
+      setUsername('');
     },
-    [username]
+    [setUsernameKey, username]
   );
 
   return (
     <div className="max-w-4xl m-auto px-2 sm:px-6 lg:px-8">
       <div className="flex justify-center w-full">
         <Card>
-          <form onSubmit={handleOnSubmit}>
+          {!!usernameKey ? (
             <div>
-              <Input
-                name="username"
-                label="Username"
-                defaultValue={username}
-                onChange={setUsername}
-                type="text"
-                required
-              />
+              <div>current user: {usernameKey}</div>
+              <div>
+                <Button onClick={removeUsernameKey}>Log out</Button>
+              </div>
             </div>
-            <div>
-              <Button type="submit">Save</Button>
-            </div>
-          </form>
+          ) : (
+            <form onSubmit={handleOnSubmit}>
+              <div>
+                <Input
+                  name="username"
+                  label="Username"
+                  defaultValue={username}
+                  onChange={setUsername}
+                  type="text"
+                  required
+                />
+              </div>
+              <div>
+                <Button type="submit">Save</Button>
+              </div>
+            </form>
+          )}
         </Card>
       </div>
     </div>
