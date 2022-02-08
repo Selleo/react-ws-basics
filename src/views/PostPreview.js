f2aa65c288b61c06d0a098bced4f013f71c9353e
michalfirszt <michalfiszu@gmail.com>
2022-02-08 15:29:04 +0100

    Post actions

--- src/views/PostPreview.js
+++ src/views/PostPreview.js
@@ -1,5 +1,5 @@
 import { isObject } from 'lodash';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { useGetPost } from '../api/posts';
@@ -9,16 +9,65 @@ export const PostPreview = () => {
   const { postId } = useParams();
   const { data } = useGetPost({ postId });
   const [post, setPost] = useState({});
+  const wsRef = useRef(null);
 
   const incrementPostLike = useCallback(() => {
-    console.log(post);
-  }, [post]);
+    wsRef.current.send(
+      JSON.stringify({
+        type: 'incrementLike',
+        payload: { post },
+      })
+    );
+  }, [post, wsRef]);
 
   useEffect(() => {
     if (isObject(data?.post)) {
       setPost(data.post);
     }
   }, [data]);
+
+  useEffect(() => {
+    if (post.id) {
+      const ws = new WebSocket('ws://localhost:8080');
+
+      ws.onopen = () => {
+        console.log('connect');
+
+        ws.send(
+          JSON.stringify({
+            type: 'connect',
+            payload: {
+              postId: post.id,
+            },
+          })
+        );
+      };
+
+      ws.onmessage = (event) => {
+        const { type, payload } = JSON.parse(event.data);
+
+        switch (type) {
+          case 'incrementLike': {
+            setPost((prevValue) => ({ ...prevValue, likes: payload.likes }));
+            break;
+          }
+          default: {
+            console.log(`Unknown response type: ${type}`);
+            break;
+          }
+        }
+      };
+
+      wsRef.current = ws;
+    }
+
+    return () => {
+      if (post.id) {
+        console.log('disconnect');
+        wsRef.current.close();
+      }
+    };
+  }, [post.id]);
 
   return (
     <div className="max-w-4xl m-auto px-2 sm:px-6 lg:px-8">
